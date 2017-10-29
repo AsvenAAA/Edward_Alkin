e2ac939d9489af920b1aeb64cc80ad168832f7f7
AsvenAAA <32891038+AsvenAAA@users.noreply.github.com>
2017-10-29 11:22:47 +1000

    Add files via upload
    Задание от 25.10.2017.

---
+++ File3.py
@@ -0,0 +1,21 @@
+File = input('File name: ') # Ввод имени файла
+
+Lines = Words = Letters = 0
+
+for i in open(File):
+    i = i[0:-1] # Подсчет колличества строк. Перенос на следующую строку не считается.
+    Lines += 1
+ 
+    for j in i: # Подсчет колличества символов. Цикл выполняет проверку на пробелы
+        if j != ' ':
+            Letters += 1
+ 
+    in_out = 0 # Подсчет колличества слов. Счетчик ищет начало и конец слова, если следующим символом является пробел, то это конец слова.
+    for j in i:
+        if j != ' ' and in_out == 0:
+            Words += 1
+            in_out = 1
+        elif j == ' ':
+            in_out = 0
+            
+print(Lines, 'lines'); print(Words, 'words'); print(Letters, 'letters')
